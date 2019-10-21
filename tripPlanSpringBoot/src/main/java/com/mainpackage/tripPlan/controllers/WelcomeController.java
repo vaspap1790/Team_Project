349c1d2f5f0d60aaf28cfff01f9412f1ac7d04e0
vaspap1790 <vaspap1790@gmail.com>
2019-10-21 17:44:29 +0300

    choices

--- tripPlanSpringBoot/src/main/java/com/mainpackage/tripPlan/controllers/WelcomeController.java
+++ tripPlanSpringBoot/src/main/java/com/mainpackage/tripPlan/controllers/WelcomeController.java
@@ -1,14 +1,30 @@
 package com.mainpackage.tripPlan.controllers;
 
-
+import com.mainpackage.tripPlan.model.User;
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 
     @GetMapping(value = "/")
-    public String welcome() {
+    public String welcome(ModelMap m, HttpSession session) {
+
+        User user = (User) session.getAttribute("user");
+
+        if (user == null) {
+            User unLoggedUser = new User();
+            session.setAttribute("user", unLoggedUser);
+        }
+        
         return "index";
     }
+
+    @GetMapping(value = "/choices")
+    public String choices() {
+        return "choices";
+    }
+
 }
